5ba1bc97c50eea8d8cb076eba543faf39a15a2d1
MacKenzie Gallant <mackenzie_kg@live.ca>
2017-04-06 01:41:40 -0400

    Revised class def, continued implementation

--- DataFileUpdater.cc
+++ DataFileUpdater.cc
@@ -1,21 +1,37 @@
 // DataFileUpdater.cc
 
 #include "DataFileUpdater.h"
+#include <ofstream>
 
 using namespace std;
 
-DataFileUpdater::DataFileUpdater(EmployeeList* empList, string filename)
-{
-	updatedList = empList;
-	outFileName = filename;	
-	
-	//...
-}
-
+DataFileUpdater::DataFileUpdater() {}
 DataFileUpdater::~DataFileUpdater() {}
 
-bool DataFileUpdater::updateFile()
+bool DataFileUpdater::updateFile(string filename, EmployeeList* empList)
 {
-	//...
+	if (!empList || empList->size() < 1) {
+		// error handling here ...
+		return false;
+	}
+	
+	// create output stream and open file 
+	// second parameter is a behaviour flag; 'trunc' means that if the file 	
+	// already exists, its contents will be deleted and replaced with new data 
+	ofstream outFile(filename, ios::trunc);
+	
+	// make sure file is actually open before trying anything
+	if (!outFile.is_open()) {
+		// error handling here ...
+		return false;
+	}
+	
+	for (int i=0; i < empList->size(); ++i) {
+		Employee* e = empList->getEmployee(i);
+		
+		outFile << e->getIDNumber();
+		//...
+	}
+	
 	return true;
 }
--- DataFileUpdater.h
+++ DataFileUpdater.h
@@ -3,9 +3,7 @@
 #ifndef DATA_FILE_UPDATER_H
 #define DATA_FILE_UPDATER_H
 
-//#include "DataTransaction.h"
 #include "EmployeeList.h"
-#include <ofstream>
 #include <string>
 
 using namespace std;
@@ -13,17 +11,14 @@ using namespace std;
 class DataFileUpdater
 {
 	public:
-		DataFileUpdater(EmployeeList* empList, string filename);
+		DataFileUpdater();
 		~DataFileUpdater();
 		
-		// use private data members (set by constructor) 
-		// to write new data file 
-		bool updateFile();
+		// use updated employee data to write new file 
+		bool updateFile(string filename, EmployeeList* empList);
 		
 	private:
-		EmployeeList* updatedList;
-		string outFileName;
-		ofstream* outFile;
+		//string outFileName;
 };
 
 #endif
